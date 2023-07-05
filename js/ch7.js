6e017578b4d1798529b7ef4721d3f128842b98ce
Mario E. Matías <mmatiasgonza@gmail.com>
2023-07-05 18:34:07 -0300

    comentarios modificados en ch7.js

--- js/ch7.js
+++ js/ch7.js
@@ -3,9 +3,9 @@
 
 //!Definir variables suma y contador, ya que son los datos que iremos recogiendo de los prompt;
 //!Crear bucle con while donde se pida un número por prompt mientras se presione aceptar;
-//!Cuando se precione cancelar (la función prompt devuelve valor null cuando se cancela), cortar el bucle;
+//!Cuando se precione cancelar (la función prompt devuelve valor null cuando se cancela), cortar el bucle utilizando la función break;
 //!Convertir los números introducidos de String a Number para evitar que los tome como texto;
-//!Establecer alerta si lo que se introduce no es un número (la función Not A Number sirve para eso);
+//!Establecer alerta si lo que se introduce no es un número (la función Not A Number sirve para eso), luego continuar el bucle utilizando la función continue;
 //!Realizar la suma y actualizar el contador (el operador += suma el valor de la derecha al de la izquierda y asigna el resultado a la variable de la izquierda. Por ejemplo, suma += numero es equivalente a suma = suma + numero.)
 //!devolver el resultado de la suma y el total de numeros intorducidos por console log
 
